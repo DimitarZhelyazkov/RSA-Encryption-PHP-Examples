bfe1128b086937fc2f6704540cbbf8035e5eac4a
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2025-02-11 16:13:37 +0000

    update

--- keysclas/EncryptionKeysGenerator.php
+++ keysclas/EncryptionKeysGenerator.php
@@ -8,14 +8,20 @@ class EncryptionKeysGenerator
 	private $q; // Prime 2
 	private $n; // Must hold the product of (p * q)
 	private $fn; // F function of N -> (p -1) * (q - 1)
-	private $encryptionKeys = []; // Array that hold all prime nuumbers that are not common factors of N and Fn
+	private $encryptionKeys = []; // Array that hold all prime nuumbers that are not common factors of N and Fn and also all of them are prime. Hold Public Encryption Keys
+	private $encryptionKeysPairs = [[]]; // Array that hold private decryption keys. Every array key respond to encryption key [ encryption key => decryption key ]
+	private $rangeFrom; // hold ranged to generate decryption key
+	private $rangeTo;
 
-	public function __construct($p,$q) {
+	public function __construct($p,$q,$rangeFrom,$rangeTo) {
 		$this->p = $p;
 		$this->q = $q;
+		$this->rangeFrom = $rangeFrom;
+		$this->rangeTo = $rangeTo;
 		$this->n = $p * $q;
 		$this->fn = ( $p - 1 ) * ( $q - 1 );
 		$this->encryptionKeys = $this->findE();
+		$this->encryptionKeysPairs = $this->findD();
 	}	
 
 	private function findE() { // Find Encription Keys return array of available public keys
@@ -37,12 +43,17 @@ class EncryptionKeysGenerator
 		return $holdprimes;
 	}
 	
-	public function findD($e = [], $rangeFrom , $rangeTo , $fn) { // Find Decryption Key for each public key given in a range
+	private function findD() { // Find Decryption Key for each public key given in a range
+		$e = [];
+		$e = $this->encryptionKeys;
+		$rangeFrom = $this->rangeFrom;
+		$rangeTo = $this->rangeTo;
+		$fn = $this->fn;
 		$hold = [[]];
 		for($i = 0; $i < count($e); $i++ ){
 			for($k = $rangeFrom; $k <= $rangeTo; $k++) {
 		 
-		  		if((($e[$i] * $k) % $fn ) === 1) { $hold[$e[$i]][] = $k; } //rework... returns values that are not equal to 1
+		  		if((($e[$i] * $k) % $fn ) === 1) { $hold[$e[$i]][] = $k; }
 			}
 		}	
 		return $hold;
@@ -57,6 +68,9 @@ class EncryptionKeysGenerator
 	}
 	public function getE() {
 		return $this->encryptionKeys;
+	}
+	public function keyPairs() {
+		return $this->encryptionKeysPairs;
 	}
 }
 ?>
--- loadkeys.php
+++ loadkeys.php
@@ -4,17 +4,14 @@
 
 require_once "keysclas/EncryptionKeysGenerator.php";
 
-$keys = new EncryptionKeysGenerator(7,19); // 7 19
-
-echo "Test :";
-echo (41*29) % 108;
+$keys = new EncryptionKeysGenerator(7,19,100,1000); // 7 19 // send 0 for random key generation Example (0,0,rangeFrom,rangeTo) => 0,0,100,1000
 
 echo $keys->results();
 print_r($keys->getE());
 
 echo "<br><br><br>";
 
-print_r($keys->findD($keys->getE(),1,100,108));
+print_r($keys->keyPairs());
 
 
  
